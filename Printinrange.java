acd8b400c436610d2501ca6dc0f3360a337eb8b5
DPaul-11 <125825050+DPaul-11@users.noreply.github.com>
2023-10-21 11:59:58 +0530

    Add files via upload

---
+++ Printinrange.java
@@ -0,0 +1,78 @@
+package Binarysearchtree;
+
+public class Printinrange {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+        }
+    }
+
+    public static Node insert(Node root, int val){
+        if(root==null){
+            root=new Node(val);
+            return root;
+        }
+        if(root.data>val){
+            root.left=insert(root.left, val);
+        }
+        else{
+            root.right=insert(root.right, val);
+        }
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    public static boolean search(Node root,int key){
+        if(root==null){
+            return false;
+        }
+        if(root.data>key){
+            return search(root.left,key);
+        }
+        else if(root.data==key){
+            return true;
+        }
+        else {
+            return search(root.right,key);
+        }
+    }
+
+    public static void printInRange(Node root,int x,int y){
+        if(root==null){
+            return;
+        }
+        if(root.data>=x && root.data<=y){
+            printInRange(root.left,x,y);
+            System.out.print(root.data+" ");
+            printInRange(root.right,x,y);
+        } else if (root.data>=y) {
+            printInRange(root.left,x,y);
+        }
+        else{
+            printInRange(root.right,x,y);
+        }
+    }
+
+    public static void main(String[] args) {
+        int values[]={8,5,3,1,4,6,10,11,14};
+        Node root=null;
+        for(int i=0;i<values.length;i++)
+        {
+            root=insert(root,values[i]);
+        }
+        inorder(root);
+        System.out.println();
+
+        printInRange(root,6,10);
+    }
+
+}
